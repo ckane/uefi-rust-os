bb282e96581d4330ceb60ab9411d70d562005ac8
Coleman Kane <ckane@colemankane.org>
2023-09-02 00:09:37 -0400

    Add latest code for completion of Blog Part 2

--- part2/uefiloader/qemu-testing/runtest.sh
+++ part2/uefiloader/qemu-testing/runtest.sh
@@ -4,7 +4,7 @@
 pushd `pwd`
 cd $(dirname $0)
 
-exec qemu-system-x86_64 -enable-kvm \
+exec qemu-system-x86_64 -enable-kvm -machine q35 \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
     -drive format=raw,file=fat:rw:esp
--- part2/uefiloader/src/main.rs
+++ part2/uefiloader/src/main.rs
@@ -1,11 +1,79 @@
 #![no_main]
 #![no_std]
 
+mod cfg_table_type;
+mod kernel_args;
+mod identity_acpi_handler;
+
 // Use the abstracted log interface for console output
 use log::{error, info, warn};
 
+use acpi::AcpiTables;
+use acpi::mcfg::PciConfigRegions;
+
 // Import a bunch of commonly-used UEFI symbols exported by the crate
 use uefi::prelude::*;
+
+use crate::kernel_args::{KernelArgs, OSMemEntry};
+use crate::identity_acpi_handler::IdentityAcpiHandler;
+
+fn wait_for_keypress(st: &mut SystemTable<Boot>) -> uefi::Result {
+    info!("Press a key to contine...");
+    st.stdin().reset(true)?;
+
+    let mut key_press_event = unsafe { [st.stdin().wait_for_key_event().unsafe_clone()] };
+    st.boot_services().wait_for_event(&mut key_press_event).unwrap();
+    Ok(())
+}
+
+impl From<&uefi::table::boot::MemoryDescriptor> for OSMemEntry {
+    fn from(mdesc: &uefi::table::boot::MemoryDescriptor) -> OSMemEntry {
+        OSMemEntry {
+            ty: mdesc.ty,
+            base: mdesc.phys_start as usize,
+            pages: mdesc.page_count as usize,
+            att: mdesc.att,
+        }
+    }
+}
+
+fn get_mm(st: &SystemTable<Boot>) -> (*mut OSMemEntry, usize) {
+    // Allocate a buffer for the memory map
+    let mm_size = st.boot_services().memory_map_size();
+
+    // Make it a few entries bigger than the size that was given
+    let mm_bytes = mm_size.map_size + (mm_size.entry_size * 5);
+    let mm_buffer = st.boot_services().allocate_pool(
+        uefi::table::boot::MemoryType::BOOT_SERVICES_DATA,
+        mm_bytes,
+    ).unwrap();
+
+    // Convert from *mut u8 to &mut [u8]
+    let mm_ref = unsafe { core::slice::from_raw_parts_mut(mm_buffer, mm_bytes) };
+
+    // Populate the memory map from UEFI into this new buffer
+    let mdesc = st.boot_services().memory_map(mm_ref).unwrap();
+
+    // Allocate a new buffer that is guaranteed to fit the same number of OSMemEntry items
+    // that we have MemoryDescriptor items for
+    let mem_entries = (mm_bytes / mm_size.entry_size) + 1;
+    let mementry_ptr = st.boot_services().allocate_pool(
+        uefi::table::boot::MemoryType::RUNTIME_SERVICES_DATA,
+        mem_entries * core::mem::size_of::<OSMemEntry>(),
+    ).unwrap() as *mut OSMemEntry;
+
+    // Convert it from a *mut OSMemEntry to a &mut [OSMemEntry] to make it safer to index
+    let mementries = unsafe { core::slice::from_raw_parts_mut::<OSMemEntry>(mementry_ptr, mem_entries) };
+
+    // Loop across the MemoryDescriptors and make a copy of each one into the &mut [OSMemEntry]
+    // slice
+    let mut num_entries = 0;
+    for (i, e) in mdesc.entries().enumerate() {
+        mementries[i] = e.into(); // Use the translation code that we wrote
+        num_entries += 1;
+    };
+    (mementry_ptr, num_entries)
+}
 
 // Tell the uefi crate that this function will be our entrypoint
 #[entry]
@@ -15,9 +83,6 @@ fn hello_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Stat
     // initialized by the UEFI program
     uefi_services::init(&mut system_table).unwrap();
 
-    info!("This is an INFO message");
-    warn!("This is a WARN message");
-    error!("This is an ERROR message");
     info!("Image Handle: {:#018x}", image_handle.as_ptr() as usize);
     info!("System Table: {:#018x}", core::ptr::addr_of!(system_table) as usize);
     info!(
@@ -26,9 +91,33 @@ fn hello_main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Stat
         system_table.uefi_revision().minor()
     );
 
-    // Pause for 10 seconds
-    system_table.boot_services().stall(10_000_000);
+    let mut karg = KernelArgs::default();
+    info!("Empty karg: {:?}", karg);
+    karg.populate_from_cfg_table(system_table.config_table());
+    info!("Populated karg: {:?}", karg);
+
+    let ih = IdentityAcpiHandler; // Create a new IdentityAcpiHandler
+    let acpi_tables = unsafe { AcpiTables::from_rsdp(ih, karg.get_acpi().0 as usize) }.unwrap();
+    info!("ACPI Revision: {}", acpi_tables.revision);
+
+    let pcie_cfg = PciConfigRegions::new(&acpi_tables).unwrap();
+    for sg in 0u16..=65535u16 {
+        if let Some(addr) = pcie_cfg.physical_address(sg, 0, 0, 0) {
+            karg.set_pcie(addr as *mut core::ffi::c_void);
+            break;
+        }
+    }
+    info!("karg after PCIe: {:?}", karg);
+
+    wait_for_keypress(&mut system_table).unwrap();
+
+    let (mm_ptr, count) = get_mm(&system_table);
+    karg.set_memmap(mm_ptr, count);
+
+    info!("Got memory");
+    info!("karg after MemMap: {:?}", karg);
 
     // Tell the UEFI firmware we exited without error
+    wait_for_keypress(&mut system_table).unwrap();
     Status::SUCCESS
 }
